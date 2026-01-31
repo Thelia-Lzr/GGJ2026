3804665b96c27cb49092173696056ebe01c9bc9c
Thelia <teddyyzr@foxmail.com>
2026-01-31 11:04:51 +0800

    1

--- GGJ/Assets/Scripts/ResourceController.cs
+++ GGJ/Assets/Scripts/ResourceController.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public static class ResourceController
+public class ResourceController: MonoBehaviour
 {
+    public static ResourceController Instance { get; private set; }
     public static Font FONT;
+    [SerializeField] public Dictionary<string, GameObject> prefabs;
+    
     static ResourceController()
     {
         FONT = Resources.Load<Font>("FONT/simsunSDF");
     }
 
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
