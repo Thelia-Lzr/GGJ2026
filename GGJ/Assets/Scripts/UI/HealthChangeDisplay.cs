e7a57080fdbd568ae2d2f71a001a299c1c1ffcac
Zhangen <zen53239@gmail.com>
2026-01-31 15:05:03 +0800

    Merge pull request #15 from Thelia-Lzr/mask
    111

--- GGJ/Assets/Scripts/UI/HealthChangeDisplay.cs
+++ GGJ/Assets/Scripts/UI/HealthChangeDisplay.cs
@@ -1,6 +1,7 @@
-using System;
+
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HealthChangeDisplay : MonoBehaviour
@@ -9,16 +10,37 @@ public class HealthChangeDisplay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        time =UnityEngine.Random.Range(0.5f,0.7f);
     }
-    public IEnumerator Display()
+    public void Intial(int amount)
     {
-        while (true)
+        StartCoroutine(Display(amount));
+    }
+    public IEnumerator Display(int amount)
+    {
+        time = Random.Range(0.5f, 1f);
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector2(100,200);
+        rectTransform.localScale = new Vector3(1, 1, 1);
+        var movePosition = new Vector2(Random.Range(-1f, 1f),Random.Range(2f, 3f));
+        //Debug.Log(movePosition);
+        TextMeshProUGUI text = GetComponent<TextMeshProUGUI>();
+        if (amount > 0)
         {
-
-
-            yield return null;
+            text.text = "+"+amount.ToString()+"!";
 
         }
+        else
+        {
+            text.text = amount.ToString()+"!";
+        }
+        text.color = Color.red;
+        text.fontSize = Random.Range(80,150);
+        while (time>0)
+        {
+            time-=Time.deltaTime;
+            rectTransform.anchoredPosition += movePosition;
+            yield return null;
+        }
+        Destroy(gameObject);
     }
 }
